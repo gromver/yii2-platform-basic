a745677edce196f35fb9e0ca3e1c71de82e1e59d
Roman <roman.tlt-84@mail.ru>
2015-01-06 19:04:26 +0400

    added phpdocs

--- modules/news/models/PostViewed.php
+++ modules/news/models/PostViewed.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @link https://github.com/gromver/yii2-platform-basic.git#readme
+ * @copyright Copyright (c) Gayazov Roman, 2014
+ * @license https://github.com/gromver/yii2-platform-basic/blob/master/LICENSE
+ * @package yii2-platform-basic
+ * @version 1.0.0
+ */
 
 namespace gromver\platform\basic\modules\news\models;
 
@@ -10,6 +17,8 @@ use yii\db\ActiveRecord;
 
 /**
  * This is the model class for table "{{%grom_post_viewed}}".
+ * @package yii2-platform-basic
+ * @author Gayazov Roman <gromver5@gmail.com>
  *
  * @property integer $post_id
  * @property integer $user_id
