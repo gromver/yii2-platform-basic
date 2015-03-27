dabce16c661b04789c849620b070f027a76b3475
Roman <roman.tlt-84@mail.ru>
2015-03-27 13:23:06 +0400

    code cleanup

--- components/BaseSearchModule.php
+++ components/BaseSearchModule.php
@@ -10,10 +10,8 @@
 namespace gromver\platform\basic\components;
 
 
-use gromver\modulequery\ModuleEvent;
 use gromver\modulequery\ModuleEventsInterface;
 use gromver\platform\basic\behaviors\SearchBehavior;
-use gromver\platform\basic\components\events\SearchableModelsEvent;
 use gromver\platform\basic\interfaces\model\SearchableInterface;
 use gromver\platform\basic\interfaces\model\ViewableInterface;
 use yii\base\InvalidParamException;
@@ -25,43 +23,6 @@ use yii\base\InvalidParamException;
  */
 class BaseSearchModule extends \yii\base\Module implements ModuleEventsInterface
 {
-    // <-- лишнее
-    const EVENT_FRONTEND_SEARCHABLE_MODELS = 'SearchableModelsFrontend';
-    const EVENT_BACKEND_SEARCHABLE_MODELS = 'SearchableModelsBackend';
-
-    public $frontendSearchableModels = [
-        'gromver\platform\basic\modules\page\models\Page',
-        'gromver\platform\basic\modules\news\models\Post',
-        'gromver\platform\basic\modules\news\models\Category',
-    ];
-
-    public $backendSearchableModels = [
-        'gromver\platform\basic\modules\page\models\Page',
-        'gromver\platform\basic\modules\news\models\Post',
-        'gromver\platform\basic\modules\news\models\Category',
-    ];
-
-    public function getFrontendSearchableModels()
-    {
-        $event = new SearchableModelsEvent([
-            'models' => $this->frontendSearchableModels
-        ]);
-        ModuleEvent::trigger(self::EVENT_FRONTEND_SEARCHABLE_MODELS, $event);
-
-        return $event->models;
-    }
-
-    public function getBackendSearchableModels()
-    {
-        $event = new SearchableModelsEvent([
-            'models' => $this->backendSearchableModels
-        ]);
-        ModuleEvent::trigger(self::EVENT_BACKEND_SEARCHABLE_MODELS, $event);
-
-        return $event->models;
-    }
-    // лишнее -->
-
     /**
      * Инедксация сохраненой модели для последующего поиска по этому индексу
      * @param \gromver\platform\basic\behaviors\SearchBehaviorEvent $event
