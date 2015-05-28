245163716096a1bbd17717c73ae12112b72d07d6
Roman <roman.tlt-84@mail.ru>
2015-05-28 22:58:12 +0400

    fix views. remove Selectize dependency, add bariew/yii2-module-migration-controller dependency

---
+++ modules/main/migrations/m140811_143606_grom_create_tables.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace gromver\platform\modules\main\migrations;
+
+use yii\db\Schema;
+
+class m140811_143606_grom_create_tables extends \yii\db\Migration
+{
+    public function up()
+    {
+        //TABLE
+        $this->createTable('{{%grom_db_state}}', [
+            'id' => Schema::TYPE_STRING . ' NOT NULL',
+            'timestamp' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'PRIMARY KEY (`id`)'
+        ]);
+    }
+
+    public function down()
+    {
+        //TABLE
+        $this->dropTable('{{%grom_db_state}}');
+    }
+}
